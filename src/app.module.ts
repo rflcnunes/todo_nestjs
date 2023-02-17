e8cb5288ad0bd3a767878bd27f58fb4a64ff0197
rafael <rafaelcnunes@icloud.com>
2023-02-17 03:03:41 -0300

    added typeorm and first entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './typeorm/entities/User';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'user',
+      password: 'password',
+      database: 'todo_db',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ length: 100 })
+  password: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  authStrategy: string;
+}
