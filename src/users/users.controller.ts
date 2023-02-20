102be7177c0ffb595d86f0c871c328b446e59b9e
rafael <rafaelcnunes@icloud.com>
2023-02-19 23:44:01 -0300

    added guards in the users routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,22 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/CreateUser.dto';
 import { UsersService } from './users.service';
 import { CreateTaskUserDto } from './dto/CreateTaskUser';
 import { User } from 'src/typeorm/entities/User';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getAllUsers(): Promise<User[]> {
     return this.userService.getAllUsers();
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async create(@Body() createUserDto: CreateUserDto) {
     const response = await this.userService.createUser(createUserDto);
 
@@ -23,6 +26,7 @@ export class UsersController {
   }
 
   @Post(':userId/tasks')
+  @UseGuards(JwtAuthGuard)
   async createTaskForUser(
     @Param('userId') userId: number,
     @Body() createTaskUserDto: CreateTaskUserDto,
