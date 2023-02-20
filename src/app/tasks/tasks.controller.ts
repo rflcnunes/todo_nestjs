b30d8d942a36bf6c83a6093cf1ad3199a5612527
rafael <rafaelcnunes@icloud.com>
2023-02-20 20:54:34 -0300

    added update task

--- src/app/tasks/tasks.controller.ts
+++ src/app/tasks/tasks.controller.ts
@@ -1,18 +1,49 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  NotFoundException,
+  UseGuards,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/CreateTaskDto';
+import { UpdateTaskDto } from './dto/UpdateTaskDto';
+import { Task } from 'src/entities/Task';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async create(@Body() createTaskDto: CreateTaskDto) {
     return await this.taskService.createTask(createTaskDto);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async findAll() {
-    return this.taskService.findAll();
+    try {
+      return this.taskService.findAll();
+    } catch (error) {
+      throw new NotFoundException('Tasks not found');
+    }
+  }
+
+  @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  async updateTask(
+    @Param('id') id: number,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    try {
+      return this.taskService.updateTask(id, updateTaskDto);
+    } catch (error) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
   }
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from 'src/entities/Task';
 import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/CreateTaskDto';
+import { UpdateTaskDto } from './dto/UpdateTaskDto';
 
 @Injectable()
 export class TasksService {
@@ -20,5 +21,25 @@ export class TasksService {
 
   async findAll(): Promise<Task[]> {
     return this.taskRepository.find({ relations: ['user'] });
+  }
+
+  async findOne(id: number): Promise<Task> {
+    return this.taskRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async updateTask(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    const task = await this.findOne(id);
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    const updatedTask = await this.taskRepository.save({
+      ...task,
+      ...updateTaskDto,
+    });
+
+    return updatedTask;
   }
 }
