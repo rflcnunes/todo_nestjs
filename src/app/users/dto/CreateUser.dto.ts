ac0d0d03db5b081c1f06f411818e28aa72333f78
rafael <rafaelcnunes@icloud.com>
2023-02-20 22:16:26 -0300

    added description swagger create user dto

--- src/app/users/dto/CreateUser.dto.ts
+++ src/app/users/dto/CreateUser.dto.ts
@@ -1,13 +1,22 @@
-import { IsNotEmpty, Matches } from 'class-validator';
+import { IsNotEmpty, Matches, MinLength } from 'class-validator';
 import { RegExHelper } from '../../../helpers/regex.helper';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The username of the user',
+    minimum: 3,
+  })
+  @MinLength(3)
   @IsNotEmpty()
   username: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'The password of the user (minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character)',
+    minimum: 8,
+    pattern: '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$',
+  })
   @IsNotEmpty()
   @Matches(RegExHelper.password)
   password: string;
