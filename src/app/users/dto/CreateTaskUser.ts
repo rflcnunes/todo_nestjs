2b1191bf6f7d312a94132dcc80a2f7e49a7e1cb2
rafael <rafaelcnunes@icloud.com>
2023-02-20 22:19:01 -0300

    added description create task user dto

--- src/app/users/dto/CreateTaskUser.ts
+++ src/app/users/dto/CreateTaskUser.ts
@@ -1,10 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { MinLength } from 'class-validator';
 
 export class CreateTaskUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The title of the task',
+    minimum: 5,
+  })
+  @MinLength(5)
   title: string;
 
-  @ApiProperty()
-  description: string;
-  createdAt?: Date;
+  @ApiProperty({
+    description: 'The description of the task',
+    minimum: 5,
+    required: false,
+  })
+  @MinLength(5)
+  description?: string;
 }
