04c6aeed6a8c554014eac9830924c24232e39ce6
rafael <rafaelcnunes@icloud.com>
2023-02-20 22:21:30 -0300

    added description in update user dto

--- src/app/users/dto/UpdateUser.ts
+++ src/app/users/dto/UpdateUser.ts
@@ -1,12 +1,23 @@
-import { Matches } from 'class-validator';
+import { Matches, MinLength } from 'class-validator';
 import { RegExHelper } from '../../../helpers/regex.helper';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The username of the user',
+    minimum: 3,
+    required: true,
+  })
+  @MinLength(3)
   username: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'The password of the user (minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character)',
+    minimum: 8,
+    required: true,
+    pattern: '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$',
+  })
   @Matches(RegExHelper.password)
   password: string;
 }
