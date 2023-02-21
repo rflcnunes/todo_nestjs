e040750be0e7cf7ca1e361ec265af744cd43a271
rafael <rafaelcnunes@icloud.com>
2023-02-20 21:01:36 -0300

    added default response

---
+++ src/app/interfaces/default.response.ts
@@ -0,0 +1,4 @@
+export interface DefaultResponse {
+  info: string;
+  data: any;
+}
--- src/app/tasks/tasks.controller.ts
+++ src/app/tasks/tasks.controller.ts
@@ -6,21 +6,24 @@ import {
   Param,
   NotFoundException,
   UseGuards,
+  HttpCode,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { UpdateTaskDto } from './dto/UpdateTaskDto';
 import { Task } from 'src/entities/Task';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { DefaultResponse } from '../interfaces/default.response';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
+  @HttpCode(200)
   @UseGuards(JwtAuthGuard)
-  async findAll() {
+  async findAll(): Promise<DefaultResponse> {
     try {
-      return this.taskService.findAll();
+      return { info: 'All tasks', data: await this.taskService.findAll() };
     } catch (error) {
       throw new NotFoundException('Tasks not found');
     }
