709ed11612506ccad21e3bfc414b17a0b5bffd15
rafael <rafaelcnunes@icloud.com>
2023-02-20 22:37:11 -0300

    added ApiExcludeController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { AuthService } from './app/auth/auth.service';
 import { JwtAuthGuard } from './app/auth/jwt-auth.guard';
 
+@ApiExcludeController()
 @Controller()
 export class AppController {
   constructor(
