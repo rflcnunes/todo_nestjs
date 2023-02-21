9ccb4d4a04a13788c42939763b8f6e699b57f767
rafael <rafaelcnunes@icloud.com>
2023-02-20 23:17:43 -0300

    add route to get token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+  Body,
+} from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { AuthService } from './app/auth/auth.service';
 import { JwtAuthGuard } from './app/auth/jwt-auth.guard';
+import { CreateUserDto } from './app/users/dto/CreateUser.dto';
+import { HashHelper } from './helpers/hash.helper';
 
 @ApiExcludeController()
 @Controller()
@@ -15,6 +24,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('auth/register')
+  async register(@Body() userDto: CreateUserDto) {
+    const { username, password } = userDto;
+    const hashedPassword = await HashHelper.hashWithBcrypt(password);
+    return this.authService.register(username, hashedPassword);
+  }
+
+  @Post('auth/authorization')
+  async authorization(@Request() req) {
+    const { username, password } = req.body;
+
+    return this.authService.login(username, password);
   }
 
   @Post('auth/login')
